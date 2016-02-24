97a13f5870141574f07cfd06d53a834545f77ee9
Oscar <oscar@nicemondays.com>
2016-02-24 16:12:14 +0100

    Added includes and nested layouts

---
+++ metal.js
@@ -0,0 +1,52 @@
+var Metalsmith = require('metalsmith'),
+	extname = require('path').extname,
+	layouts = require('metalsmith-layouts'),
+	multiLanguage = require('metalsmith-multi-language'),
+	permalinks = require('metalsmith-permalinks'),
+	i18n = require('metalsmith-i18n'),
+	collections = require('metalsmith-collections'),
+    define = require('metalsmith-define'),
+    beautify = require('metalsmith-beautify'),
+    pkg = require('./package.json');
+
+const 	DEFAULT_LOCALE = 'es',
+		LOCALES = ['en', 'es'];
+
+var metalsmith = Metalsmith(__dirname)
+	.source('src/pages')
+	.destination('dist')
+    .use(define({
+        getLayout: function(_name){
+            return pkg.project.layouts_folder + _name + '.j2';
+        },
+        getInclude: function(_name) {
+            return pkg.project.layouts_folder + 'includes/' + _name + '.j2';
+        }
+    }))
+	.use(collections({
+		'pages_es': '*_es.html',
+		'pages_en': '*_en.html',
+	}))
+	.use(multiLanguage({
+        default: DEFAULT_LOCALE,
+        locales: LOCALES
+    }))
+    .use(i18n({
+        default: DEFAULT_LOCALE,
+        locales: LOCALES,
+        directory: 'src/locales'
+    }))
+    .use(permalinks({
+        relative: false,
+        pattern: ':locale/:title/'
+    }))
+	.use(layouts({
+        engine: 'nunjucks',
+        default: 'default.j2',
+        pattern: '**/*.html',
+        directory: 'src/layouts'
+    }))
+    .use(beautify())
+    .build(function(err, files){
+        if (err) console.error(err.stack)
+    });
